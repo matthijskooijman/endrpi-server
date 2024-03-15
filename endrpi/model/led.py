3179dc4af0b727b4651c0f8beab16b90353ce657
Matthijs Kooijman <matthijs@stdin.nl>
2024-03-15 18:50:43 +0100

    Fix copyright notice

--- endrpi/model/led.py
+++ endrpi/model/led.py
@@ -1,4 +1,4 @@
-#  Copyright (c) 2020 - 2021 Persanix LLC. All rights reserved.
+#  Copyright (c) 2023 Matthijs Kooijman
 #
 #  Licensed under the Apache License, Version 2.0 (the "License");
 #  you may not use this file except in compliance with the License.
